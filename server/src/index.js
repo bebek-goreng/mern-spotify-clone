1e2ce578ee5ea936fac14998e76102ea230f192e
bebek-goreng <oresuki01@gmail.com>
2024-11-12 09:24:45 +0700

    add: feature cloudinary & fileupload

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { clerkMiddleware } from '@clerk/express';
+import fileUpload from 'express-fileupload';
+import path from 'path';
 import { connectionDB } from './db/db.js';
 import { route } from './routes/main.route.js';
 import { errorHandler } from './middlewares/error.handler.js';
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -17,7 +20,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+        fileSize: 10 * 1024 * 1024
+    }
+}));
 app.use(route);
 app.use(errorHandler);
 
