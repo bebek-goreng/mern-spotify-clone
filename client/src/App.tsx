b44b1d3ae68100961ae927d5ecd02ac264aab525
bebek-goreng <oresuki01@gmail.com>
2024-12-27 12:16:26 +0700

    add: dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,30 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import AuthCallback from "./pages/auth-callback/AuthCallbackPage";
+import HomePage from "./pages/home/HomePage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
     <>
       <header>
-        <SignedOut>
-          <SignInButton>
-            <Button>
-              Sign In
-            </Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <Routes>
+          <Route
+            path="/sso-callback"
+            element={
+              <AuthenticateWithRedirectCallback
+                signInForceRedirectUrl={"/auth-callback"}
+              />
+            }
+          />
+          <Route path="/auth-callback" element={<AuthCallback />} />
+
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/chat" element={<ChatPage />} />
+          </Route>
+        </Routes>
       </header>
     </>
   );
