68729f491d65c99c23afb00a9dfe27373a317f5e
bebek-goreng <oresuki01@gmail.com>
2024-12-31 13:25:44 +0700

    add: song seeder file

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,12 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true
+    }
+));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
