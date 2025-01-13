a5899665159a0efe766d713ab599a27592a12ffd
bebek-goreng <oresuki01@gmail.com>
2025-01-13 12:05:53 +0700

    edit: typo in server/index.js

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const connectionDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Connented to MongoDB: ${conn.connection.host}`);
+        console.log(`Connected to MongoDB: ${conn.connection.host}`);
     } catch (error) {
         console.log(`Failed to connect in MongoDB`, error);
         process.exit(1);
